4db363712d6aae68d23f3c1d5b20a8502452bea7
Efecancnbz <profilo12aefecan@gmail.com>
2024-12-16 12:07:55 +0300

    Add files via upload

---
+++ AMP12A_922_StokTakipProgram-main/AMP12A_922_StokTakipProgram/Model/Personel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMP12A_922_StokTakipProgram.Model
+{
+    public class Personel
+    {
+        public int Id { get; set; }
+
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+
+        public string Telefon { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Adres { get; set; }
+
+        public string KullaniciAdi { get; set; }
+
+        public string Sifre { get; set; }
+
+
+        [ForeignKey("Yetki")]
+
+        public int YetkiId { get; set; }
+
+
+
+        public virtual Yetki Yetki { get; set; }
+
+        public virtual ICollection<Satis> Satislar { get; set; } = new HashSet<Satis>();
+    }
+}
